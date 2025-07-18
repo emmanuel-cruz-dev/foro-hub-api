2ef24ba8a25d83ca3762082ec065edc2a9f8b655
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-18 13:35:36 -0300

    feat: adds DataRegisterTopic record

---
+++ src/main/java/foro/hub/api/topic/DataRegisterTopic.java
@@ -0,0 +1,13 @@
+package foro.hub.api.topic;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DataRegisterTopic(
+        @NotNull @JsonProperty("idUsuario") Long idUser,
+        @NotBlank @JsonProperty("mensaje") String message,
+        @NotBlank @JsonProperty("nombreCurso") String nameCourse,
+        @NotBlank @JsonProperty("titulo") String title
+) {
+}
