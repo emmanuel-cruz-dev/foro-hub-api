8976f5d61fc4b4d9ab9540a07368d8719e3d4926
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-18 16:45:51 -0300

    feat: adds Getter, NoArgsConstructor and AllArgsConstructor to Curso class

--- src/main/java/foro/hub/api/domain/curso/Curso.java
+++ src/main/java/foro/hub/api/domain/curso/Curso.java
@@ -2,7 +2,13 @@ package foro.hub.api.domain.curso;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 @Embeddable
 public class Curso {
     @Column(name = "nombreCurso")
