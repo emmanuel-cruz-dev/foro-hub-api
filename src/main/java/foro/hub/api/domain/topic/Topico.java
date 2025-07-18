8931066faaf08ab92a3f4545c8be5af5f77f8007
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-18 16:07:27 -0300

    feat: adds annotations to Topico class

---
+++ src/main/java/foro/hub/api/domain/topic/Topico.java
@@ -0,0 +1,32 @@
+package foro.hub.api.domain.topic;
+
+import foro.hub.api.domain.curso.Curso;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "topicos")
+@Entity(name = "Topico")
+public class Topico {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String titulo;
+    private String mensaje;
+
+    @Embedded
+    private Curso nombreCurso;
+
+    private Long idAutor;
+    private LocalDateTime fechaCreacion;
+    private boolean status;
+}
