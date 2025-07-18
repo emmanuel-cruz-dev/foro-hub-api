32e5cdfb868dc5dd0688dfb1fdb7de4dfd4e9c1a
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-18 16:06:18 -0300

    refactor: renames DataRegisterTopic to DatosRegistroTopico

---
+++ src/main/java/foro/hub/api/domain/topic/DatosRegistroTopico.java
@@ -0,0 +1,12 @@
+package foro.hub.api.domain.topic;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record DatosRegistroTopico(
+        @NotNull String idAutor,
+        @NotBlank String mensaje,
+        @NotBlank String nombreCurso,
+        @NotBlank String titulo
+) {
+}
