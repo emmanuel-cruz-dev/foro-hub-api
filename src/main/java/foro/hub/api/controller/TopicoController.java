9cd8a8a05aa640648b141ac14d0f3880443ed83d
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-18 16:44:09 -0300

    feat: adds TopicoRepository to TopicoController

--- src/main/java/foro/hub/api/controller/TopicoController.java
+++ src/main/java/foro/hub/api/controller/TopicoController.java
@@ -1,14 +1,20 @@
 package foro.hub.api.controller;
 
 import foro.hub.api.domain.topic.DatosRegistroTopico;
+import foro.hub.api.domain.topic.Topico;
+import foro.hub.api.domain.topic.TopicoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/topicos")
 public class TopicoController {
 
+    @Autowired
+    private TopicoRepository repository;
+
     @PostMapping
-    public void registrar(@RequestBody DatosRegistroTopico data) {
-        System.out.println(data);
+    public void registrar(@RequestBody DatosRegistroTopico datos) {
+        repository.save(new Topico(datos));
     }
 }
