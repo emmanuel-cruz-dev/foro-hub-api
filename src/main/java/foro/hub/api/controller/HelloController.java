1d564fa5e9c54cc59fa8ef61e949cdf1b11650f2
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-19 16:26:08 -0300

    refactor: modifies HelloController content

--- src/main/java/foro/hub/api/controller/HelloController.java
+++ src/main/java/foro/hub/api/controller/HelloController.java
@@ -5,11 +5,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
+@RequestMapping
 public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "Home Page. Hello World";
+        return "Hello World";
     }
 }
