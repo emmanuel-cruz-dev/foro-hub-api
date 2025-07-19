67155a3c2dbdae9247407f8b52025d4bf3094104
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-19 16:00:19 -0300

    feat: adds manejarErroresDeDuplicados method to GestorDeErrores class

--- src/main/java/foro/hub/api/infra/exceptions/GestorDeErrores.java
+++ src/main/java/foro/hub/api/infra/exceptions/GestorDeErrores.java
@@ -1,5 +1,6 @@
 package foro.hub.api.infra.exceptions;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -11,5 +12,13 @@ public class GestorDeErrores {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<String> manejarErroresDeValidacion(MethodArgumentNotValidException ex) {
         return ResponseEntity.badRequest().body("Error: Campos inválidos o faltantes");
+    }
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<String> manejarErroresDeDuplicados(DataIntegrityViolationException ex) {
+        if (ex.getMessage().contains("titulo") || ex.getMessage().contains("mensaje")) {
+            return ResponseEntity.badRequest().body("Error: Ya existe un tópico con el mismo título y mensaje");
+        }
+        return ResponseEntity.badRequest().body("Error: Violación de integridad de datos");
     }
 }
