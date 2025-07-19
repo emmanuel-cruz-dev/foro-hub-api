1774a18f68192fd6ae45e8e804b1ed3828d83162
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-19 17:44:02 -0300

    feat: adds Usuario class

---
+++ src/main/java/foro/hub/api/domain/usuario/Usuario.java
@@ -0,0 +1,21 @@
+package foro.hub.api.domain.usuario;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Table(name = "usuarios")
+@Entity(name = "Usuario")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String contrasena;
+}
