4959d3aa122072c94021a188418e7baef176109f
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-19 13:09:49 -0300

    chore: removes unnecessary Curso and DatosCurso java files

--- src/main/java/foro/hub/api/domain/curso/DatosCurso.java
+++
@@ -1,9 +0,0 @@
-package foro.hub.api.domain.curso;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record DatosCurso(
-        @NotBlank String nombreCurso,
-        @NotBlank String categoria
-) {
-}
