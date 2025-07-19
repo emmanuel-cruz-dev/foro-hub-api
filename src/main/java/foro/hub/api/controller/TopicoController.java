d9866028c7fbeec266165789fe942d564ee3e046
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-19 11:54:06 -0300

    feat: adds listar GET method to TopicoController class

--- src/main/java/foro/hub/api/controller/TopicoController.java
+++ src/main/java/foro/hub/api/controller/TopicoController.java
@@ -1,10 +1,13 @@
 package foro.hub.api.controller;
 
-import foro.hub.api.domain.topic.DatosRegistroTopico;
-import foro.hub.api.domain.topic.Topico;
-import foro.hub.api.domain.topic.TopicoRepository;
+import foro.hub.api.domain.topico.DatosRegistroTopico;
+import foro.hub.api.domain.topico.Topico;
+import foro.hub.api.domain.topico.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/topicos")
@@ -12,6 +15,11 @@ public class TopicoController {
 
     @Autowired
     private TopicoRepository repository;
+
+    @GetMapping
+    public ResponseEntity<List<Topico>> listar() {
+        return ResponseEntity.ok(repository.findAll());
+    }
 
     @PostMapping
     public void registrar(@RequestBody DatosRegistroTopico datos) {
