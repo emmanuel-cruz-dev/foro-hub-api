299e846acfbabca2f9c2dc07260636eb638028cb
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-20 15:27:07 -0300

    refactor: updates email to login private variable in Usuario class

--- src/main/java/foro/hub/api/domain/usuario/Usuario.java
+++ src/main/java/foro/hub/api/domain/usuario/Usuario.java
@@ -16,6 +16,6 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String email;
+    private String login;
     private String contrasena;
 }
