8f19ab0e3c1951efebab9e4977d62cd939edbd42
Emmanuel Cruz <155833447+emmanuel-cruz-dev@users.noreply.github.com>
2025-07-20 17:31:20 -0300

    feat: updates functionality in iniciarSesion method by adding tokenService in AutenticacionController class

--- src/main/java/foro/hub/api/controller/AutenticacionController.java
+++ src/main/java/foro/hub/api/controller/AutenticacionController.java
@@ -2,6 +2,7 @@ package foro.hub.api.controller;
 
 import foro.hub.api.domain.usuario.DatosAutenticacion;
 import foro.hub.api.domain.usuario.Usuario;
+import foro.hub.api.infra.security.DatosTokenJWT;
 import foro.hub.api.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,9 +26,10 @@ public class AutenticacionController {
 
     @PostMapping
     public ResponseEntity iniciarSesion(@RequestBody @Valid DatosAutenticacion datos) {
-        var token = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
-        var autenticacion = manager.authenticate(token);
+        var authenticationToken = new UsernamePasswordAuthenticationToken(datos.login(), datos.contrasena());
+        var autenticacion = manager.authenticate(authenticationToken);
 
-        return ResponseEntity.ok(tokenService.generarToken((Usuario) autenticacion.getPrincipal()));
+        var tokenJWT = tokenService.generarToken((Usuario) autenticacion.getPrincipal());
+        return ResponseEntity.ok(new DatosTokenJWT(tokenJWT));
     }
 }
